5cc06535a7c39b224748121eb2bccfc1d4be5977
Hezzie <hezronchelimo.hc@gmail.com>
2024-11-20 05:26:25 +0300

    update on the prisma schema and migrations

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '../prisma/client'
 
 export const getAllUsers = async () => {
   return prisma.user.findMany();

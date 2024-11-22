bc9b0021d0fe0228d938e630260196cf03691db7
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-11-22 17:41:47 +0300

    cors and frontend connection

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,34 @@
-import express from 'express';
-import userRoutes from './routes/user.routes';
-import eventRoutes from './routes/event.routes';
-import ticketRoutes from './routes/ticket.routes';
-import ticketCategoriesRoutes from './routes/ticketCategories.routes';
+import express from "express";
+import userRoutes from "./routes/user.routes";
+import eventRoutes from "./routes/event.routes";
+import ticketRoutes from "./routes/ticket.routes";
+import ticketCategoriesRoutes from "./routes/ticketCategories.routes";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: "http://localhost:8080",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.get('/health', (req, res) => {
-    res.status(200).json({ message: 'Server is healthy' });
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Server is healthy" });
+  
 });
-app.use('/api/users', userRoutes);
-app.use('/api/events', eventRoutes);
-app.use('/api/tickets', ticketRoutes);
-app.use('/api/ticket-categories', ticketCategoriesRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/events", eventRoutes);
+app.use("/api/tickets", ticketRoutes);
+app.use("/api/ticket-categories", ticketCategoriesRoutes);
 
-if (process.env.NODE_ENV !== 'test') {
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
-} 
-
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
 
 export default app;
